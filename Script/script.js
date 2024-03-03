8d4f37fa224f4f232b796cf55abf38c0a5ee32fb
Dharav0126 <dharav2626@gmail.com>
2024-03-02 20:46:13 -0500

    updated portfolio

--- Script/script.js
+++ Script/script.js
@@ -1,20 +1,9 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav menu li a');
+function Showsidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display= 'flex';
+}
 
-window.onscroll = () => {
-    let fromTop = window.scrollY + 150;
-
-    navLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
-
-        if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.offsetHeight > fromTop
-        ) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            link.classList.add('active');
-        }
-    });
-};
+function Closesidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none';
+}
