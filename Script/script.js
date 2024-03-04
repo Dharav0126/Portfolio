941e7aeb43d9dac9a6c862dcd35e9eacd2f5267b
Dharav0126 <dharav2626@gmail.com>
2024-03-04 11:23:39 -0500

    closebutton functionality

--- Script/script.js
+++ Script/script.js
@@ -7,3 +7,18 @@ function Closesidebar(){
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = 'none';
 }
+
+function Toggleform(){
+    var formcontainer = document.getElementById("form-container");
+    if(formcontainer.style.display==="none"){
+        formcontainer.style.display="block";
+    }
+    else{
+        formcontainer.style.display="none";
+    }
+}
+
+function Closeform(){
+    var formcontainer = document.getElementById("form-container");
+    formcontainer.style.display="none";
+}
